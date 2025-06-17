50fe9776f41e0b681a0eb64e78d77d6267b43abb
Aayushsoni-1 <parekhaayush145@gmail.com>
2025-06-17 18:08:24 +0530

    Added Eventstimeline to the Events page

--- src/components/Events/EventsPage.jsx
+++ src/components/Events/EventsPage.jsx
@@ -31,15 +31,6 @@ export default function EventsPage() {
           </h2>
           <Timeline events={events} />
         </div>
-        <br/>
-        <div className="mb-12 flex w-full flex-wrap">
-          <div className="mb-6 w-full lg:mb-0 lg:w-1/2 2xl:pl-4">
-            <h1 className="mb-2 font-Bebas text-4xl font-medium text-primary sm:text-5xl ">
-              Our Upcoming Events
-            </h1>
-            <div className="h-1 w-20 rounded bg-primary"></div>
-          </div>
-        </div>
         <UpcomingEventCards />
       </div>
       <div className="container mx-auto px-5 pb-12">
