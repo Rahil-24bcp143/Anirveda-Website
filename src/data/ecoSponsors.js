e5386860e3906b9ec368a36866cdb6f601f75436
MannD25 <manndesai25@gmail.com>
2026-03-15 12:11:43 +0530

    sponsors added

--- src/data/ecoSponsors.js
+++ src/data/ecoSponsors.js
@@ -1,11 +1,15 @@
 import img1608 from "./IMG_1608.PNG";
+import img1609 from "Fateh_logo.jpeg";
+import img1610 from "Moksh_logo.jpeg";
+import img1611 from "UnionBank_logo.jpeg";
+import img1612 from "The_1_percent.jpeg";
 export const sponsors = [
     {
       id: 1,
       img: img1608,
       title: "The Hanana Vibes",
       description:
-        "GVFL transforms visionary ideas into world-changing realities. As India's first venture capital firm, we've been fueling the nation's entrepreneurial spirit for over three decades. ",
+        "The Hanana Vibes, Gandhinagar is a vibrant brand known for bringing creativity, style, and positive energy to the community. With a focus on quality and innovation, it aims to create memorable experiences and inspire people through its unique offerings.",
     //   website: "https://www.indusind.com/in/en/personal.html",
     //   instagram:"https://www.instagram.com/indusind_bank/",
     },
@@ -21,12 +25,39 @@ export const sponsors = [
     {
       id: 3,
       img: "https://res.cloudinary.com/duygdcgj3/image/upload/v1756113270/whitecarbon_tuhgd6.png",
-      title: "The 1% Club",
+      title: "White Carbon",
       description:
         "Founded in 2020, White Carbon Motors is putting energy into things that matter. With a mission to deliver consumer innovations that will improve how the world’s most populated cities distribute and utilize energy, the company is enabling the transformation of mega cities into smart cities.",
     //   website: "https://www.indusind.com/in/en/personal.html",
     //   instagram:"https://www.instagram.com/indusind_bank/",
     },
-    
+    {
+      id: 4,
+      img: img1612,
+      title: "The One Percent Club Fitness Studio",
+      description:
+        "The 1% Club Fitness Studio, located in GIFT City, Gandhinagar, Gujarat, is a premium gym offering high-end, cutting-edge strength and cardio equipment in a spacious, top-notch environment.",
+    },
+    {
+      id: 5,
+      img: img1609,
+      title: "FATEH",
+      description:
+        "Fateh Education is a premier overseas consultancy that has helped over 40,000 students conquer their dreams of studying in the UK and Ireland since 2004.",
+    },
+    {
+      id: 6,
+      img: img1610,
+      title: "Moksh Group",
+      description:
+        "The Moksh Group, Gandhinagar is a growing organization known for its commitment to quality, innovation, and excellence. The group supports initiatives that promote development, creativity, and community growth.",
+    },
+    {
+      id: 7, 
+      img: img1611,
+      title: "Union Bank of India",
+      description:
+        "Union Bank of India, a leading public sector bank in India, offers a wide range of financial products and services to individuals and businesses, with a strong focus on customer satisfaction and innovation.",
+    }
 ]
   
